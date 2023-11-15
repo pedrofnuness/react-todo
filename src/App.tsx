ab67621e8dc9562c358f3699e6ece6ec349d5aa1
Pedro Nunes <pedronunes.dev@gmail.com>
2023-11-15 16:27:57 -0300

    feat: implemented form to main screen and add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import React, { useState } from 'react';
 
 import Header from './components/Header/Header'
-
+import { NewTaskForm } from './components/NewTaskForm/NewTaskForm.tsx'
+import styles from './App.module.css';
 import './global.css'
 
 function App() {
+  const [taskList, setTaskList] = useState<string[]>([]);
+  
+  const handleCreateTask = (inputValue: string) => {
+    setTaskList((prevState) => [...prevState, inputValue])
+  };
+
   return (
     <>
       <Header />
+
+      <main className={styles.content}>
+        <NewTaskForm onSubmit={handleCreateTask}/>
+    
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
